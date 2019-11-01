a90427a18e7e1b1ea88a841ee9fcdf35db1ed69e
chengxuefeng <chengxuefeng@xdf.cn>
2019-11-01 11:23:13 +0800

    增加utils

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import vView from 'vue-view-lazy'
-// import ElementUI from 'element-ui';
+// import vView from 'vue-view-lazy'
+import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 // import ViewUI from 'view-design';
 // import 'view-design/dist/styles/iview.css';
 Vue.use(ElementUI);
-Vue.use(vView);
+// Vue.use(vView);
 
 // Vue.use(vView,{
 //   error:'../../static/images/loading.png',
