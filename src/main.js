124400dfa50ef0d4de671b0fc1e30f72f2befa2c
LWangR <17600765487@163.com>
2019-11-01 10:26:55 +0800

    zuixin

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import vView from 'vue-view-lazy'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
 // import ViewUI from 'view-design';
 // import 'view-design/dist/styles/iview.css';
 Vue.use(ElementUI);
+Vue.use(vView);
+
+// Vue.use(vView,{
+//   error:'../../static/images/loading.png',
+//   loading:'../../static/images/loading.gif',
+// });
 
 Vue.config.productionTip = false
 
